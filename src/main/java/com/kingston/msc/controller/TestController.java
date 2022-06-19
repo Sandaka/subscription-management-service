8a44acbf770b6b2c44906e104efbccf4b2bae1e2
Sandaka Geethaj Wijesinghe <5172640-sandaka@users.noreply.gitlab.com>
2022-06-19 19:22:23 +0530

    first commit

---
+++ src/main/java/com/kingston/msc/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.kingston.msc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Sandaka Wijesinghe.
+ * Date: 6/19/22
+ */
+@RestController
+@RequestMapping("/sms")
+public class TestController {
+}
---
+++ src/test/java/com/kingston/msc/subscriptionmanagementservice/SubscriptionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingston.msc.subscriptionmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
